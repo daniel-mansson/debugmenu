8118a937745aa8548115908e7b89387ea5b3971e
Daniel Månsson <daniel.maansson@gmail.com>
2024-01-07 23:07:48 +0100

    more ui

--- debug-menu-web2/src/routes/+page.svelte
+++ debug-menu-web2/src/routes/+page.svelte
@@ -4,21 +4,20 @@
 	import { Separator } from '$lib/components/ui/separator';
 	import icon from '$lib/assets/up_arrow.svg';
 	import { Button } from '$lib/components/ui/button';
-	import * as Card from '$lib/components/ui/card';
 </script>
 
 <div class="mx-auto my-2 w-10/12 self-center">
-	<nav class="border-gray-200 bg-white dark:bg-gray-900">
+	<nav class="border-gray-200">
 		<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
 			<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-				<img src={icon} class="h-8" alt="Flowbite Logo" />
+				<img src={icon} class="h-8 dark:invert" alt="Flowbite Logo" />
 				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
 					>DEBUGMENU.IO</span
 				>
 			</a>
 
 			<div class=" flex flex-col space-x-reverse md:w-auto" id="navbar-default">
-				<Button variant="outline" class="active:outline active:outline-1">Launch app</Button>
+				<Button href="/app" variant="outline" class="">Launch app</Button>
 			</div>
 		</div>
 	</nav>
@@ -26,12 +25,9 @@
 
 	<div class=" my-48 text-center">
 		<h1 class="mx-auto scroll-m-20 pb-2 text-4xl font-semibold">
-			Hasdf ASdfasdas Dasd fasdfasdas d
+			Control your application from anywhere
 		</h1>
+		<Button href="/app">Get started</Button>
 	</div>
 	<Separator />
-	<!-- <div
-		class="h-80 w-full rounded-bl-sm
-  rounded-br-sm bg-gradient-to-b from-slate-50 to-transparent"
-	></div> -->
 </div>
