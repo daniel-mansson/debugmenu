9e07a878824065b5d2253617692665f71dd168e3
Daniel Månsson <daniel.maansson@gmail.com>
2024-02-02 22:24:11 +0100

    added get users in team

---
+++ DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetUsersInTeam/GetUsersInTeamHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DebugMenu.Silo.Web.Teams.Persistence;
+using MediatR;
+
+namespace DebugMenu.Silo.Web.Teams.Requests.GetUsersInTeam;
+
+public class GetUsersInTeamHandler : IRequestHandler<GetUsersInTeamRequest, IReadOnlyList<TeamUserDto>> {
+    private readonly ITeamsRepository _teamsRepository;
+    private readonly IMapper _mapper;
+
+    public GetUsersInTeamHandler(ITeamsRepository teamsRepository, IMapper mapper) {
+        _teamsRepository = teamsRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<TeamUserDto>> Handle(GetUsersInTeamRequest request, CancellationToken cancellationToken) {
+        var users = await _teamsRepository.GetUsersInTeamAsync(request.TeamId);
+        return _mapper.Map<IReadOnlyList<TeamUserDto>>(users);
+    }
+}
--- DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetUsersInTeam/GetUsersInTeamRequest.cs
+++ DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetUsersInTeam/GetUsersInTeamRequest.cs
@@ -3,11 +3,5 @@
 namespace DebugMenu.Silo.Web.Teams.Requests.GetUsersInTeam;
 
 public class GetUsersInTeamRequest : IRequest<IReadOnlyList<TeamUserDto>> {
-
-}
-
-public class GetAllTeamsHandler : IRequestHandler<GetUsersInTeamRequest, IReadOnlyList<TeamUserDto>> {
-    public Task<IReadOnlyList<TeamUserDto>> Handle(GetUsersInTeamRequest request, CancellationToken cancellationToken) {
-        throw new NotImplementedException();
-    }
+    public int TeamId { get; set; }
 }
