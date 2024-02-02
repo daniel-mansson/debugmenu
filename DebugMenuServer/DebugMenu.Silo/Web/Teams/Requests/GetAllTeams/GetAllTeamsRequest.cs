38d67a7dc7bdf3da5a17d02f7979ae2854c9e719
Daniel Månsson <daniel.maansson@gmail.com>
2024-02-02 22:18:22 +0100

    added getallteams

--- DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetAllTeams/GetAllTeamsRequest.cs
+++ DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetAllTeams/GetAllTeamsRequest.cs
@@ -1,4 +1,6 @@
-﻿using DebugMenu.Silo.Web.Teams.Requests.CreateTeam;
+﻿using AutoMapper;
+using DebugMenu.Silo.Web.Teams.Persistence;
+using DebugMenu.Silo.Web.Teams.Requests.CreateTeam;
 using MediatR;
 
 namespace DebugMenu.Silo.Web.Teams.Requests.GetAllTeams;
@@ -8,7 +10,17 @@ public class GetAllTeamsRequest : IRequest<IReadOnlyList<TeamDto>> {
 }
 
 public class GetAllTeamsHandler : IRequestHandler<GetAllTeamsRequest, IReadOnlyList<TeamDto>> {
-    public Task<IReadOnlyList<TeamDto>> Handle(GetAllTeamsRequest request, CancellationToken cancellationToken) {
-        throw new NotImplementedException();
+    private readonly ITeamsRepository _teamsRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllTeamsHandler(ITeamsRepository teamsRepository, IMapper mapper) {
+        _teamsRepository = teamsRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<TeamDto>> Handle(GetAllTeamsRequest request, CancellationToken cancellationToken) {
+        //TODO: validate super user
+        var teams =  await _teamsRepository.GetAsync();
+        return _mapper.Map<IReadOnlyList<TeamDto>>(teams);
     }
 }
--- DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetTeam/GetTeamRequest.cs
+++ DebugMenuServer/DebugMenu.Silo/Web/Teams/Requests/GetTeam/GetTeamRequest.cs
@@ -1,14 +1,26 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using DebugMenu.Silo.Web.Teams.Persistence;
+using MediatR;
 
 namespace DebugMenu.Silo.Web.Teams.Requests.GetTeam;
 
 
 public class GetTeamRequest : IRequest<TeamDto> {
-
+    public int Id { get; set; }
 }
 
-public class GetAllTeamsHandler : IRequestHandler<GetTeamRequest, TeamDto> {
-    public Task<TeamDto> Handle(GetTeamRequest request, CancellationToken cancellationToken) {
-        throw new NotImplementedException();
+public class GetTeamHandler : IRequestHandler<GetTeamRequest, TeamDto> {
+    private readonly ITeamsRepository _teamsRepository;
+    private readonly IMapper _mapper;
+
+    public GetTeamHandler(ITeamsRepository teamsRepository, IMapper mapper) {
+        _teamsRepository = teamsRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<TeamDto> Handle(GetTeamRequest request, CancellationToken cancellationToken) {
+        //TODO: validate current user is in team
+        var team = await _teamsRepository.GetByIdAsync(request.Id);
+        return _mapper.Map<TeamDto>(team);
     }
 }
