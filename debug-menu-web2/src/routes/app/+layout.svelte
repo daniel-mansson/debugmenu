5844fd11d1d855c5d78f7a4e4fc6dd6d4254fa2b
Daniel Månsson <daniel.maansson@gmail.com>
2024-02-13 00:33:04 +0100

    default to visible sidebar

--- debug-menu-web2/src/routes/app/+layout.svelte
+++ debug-menu-web2/src/routes/app/+layout.svelte
@@ -6,14 +6,21 @@
 	import { currentApplication, currentInstance, currentUser, currentTeam } from '$lib/appstate';
 	import Breadcrumbs from '$lib/components/skeleton/navbar/breadcrumbs.svelte';
 	import ConnectionStatus from '$lib/components/skeleton/navbar/connection-status.svelte';
+	import { onMount } from 'svelte';
 
-	let sidebarVisible = false;
+	let screenWidth: number;
+	onMount(() => {
+		sidebarVisible = screenWidth >= 640;
+	});
+	let sidebarVisible = true;
 	function toggleSidebar() {
 		sidebarVisible = !sidebarVisible;
 	}
 
 	export let data;
 </script>
+
+<svelte:window bind:innerWidth={screenWidth} />
 
 <Navbar on:menuClicked={toggleSidebar}>
 	<Breadcrumbs slot="route" {...data}></Breadcrumbs>
