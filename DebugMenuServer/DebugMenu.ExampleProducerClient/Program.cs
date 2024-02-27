37345541bec6f3453cde30717fcae8678a8376b8
Daniel Månsson <daniel.maansson@gmail.com>
2024-02-27 19:42:54 +0100

    api

--- DebugMenuServer/DebugMenu.ExampleProducerClient/Program.cs
+++ DebugMenuServer/DebugMenu.ExampleProducerClient/Program.cs
@@ -101,102 +101,79 @@ while (true) {
 
 const string api1 = """"
   {
-  "asyncapi": "2.6.0",
-"info": {
-  "title": "Example AsyncAPI specification",
-  "version": "0.1.0"
-},
-"channels": {
-  "log": {
-    "subscribe": {
-      "tags": [
-      {
-        "name": "log"
-      }
-      ],
-      "message": {
-        "payload": {
-          "type": "object",
-          "properties": {
-            "text": {
-              "type": "string"
-            },
-            "details": {
-              "type": "string"
-            },
-            "type": {
-              "type": "string"
-            }
+  "debugmenuapi": "1.0.0",
+  "channels": {
+    "log": {
+      "name": "Log",
+      "type": "log",
+      "subscribe": {
+        "type": "object",
+        "properties": {
+          "text": {
+            "type": "string"
+          },
+          "details": {
+            "type": "string"
+          },
+          "type": {
+            "type": "string"
           }
         }
       }
-    }
-  },
-  "gameplay/spawn": {
-    "publish": {
-      "description": "asdf",
-      "tags": [
-      {
-        "name": "button"
-      }
-      ],
-      "message": {
-        "payload": {
-          "type": "object",
-          "properties": {
-            "exampleField": {
-              "type": "string",
-              "description": "This is an example text field"
-            },
-            "exampleNumber": {
-              "type": "number"
-            },
-            "exampleDate": {
-              "type": "string",
-              "format": "date-time"
-            }
+    },
+    "gameplay/spawn": {
+      "name": "Spawn",
+      "category": "Commands",
+      "type": "button",
+      "settings": {
+        "color": "red"
+      },
+      "publish": {
+        "type": "object",
+        "properties": {
+          "exampleField": {
+            "type": "string",
+            "description": "This is an example text field"
+          },
+          "exampleNumber": {
+            "type": "number"
+          },
+          "exampleDate": {
+            "type": "string",
+            "format": "date-time"
           }
         }
       }
-    }
-  },
-  "gameplay/restart": {
-    "publish": {
-      "tags": [
-      {
-        "name": "button"
-      }
-      ]
-    }
-  },
-  "progression/level-up": {
-    "publish": {
-      "tags": [
-      {
-        "name": "button"
-      }
-      ]
-    }
-  },
-  "progression/reset": {
-    "publish": {
-      "tags": [
-      {
-        "name": "button"
-      }
-      ]
-    }
-  },
-  "progression/add-xp": {
-    "publish": {
-      "tags": [
-      {
-        "name": "button"
-      }
-      ]
+    },
+    "gameplay/restart": {
+      "type": "button",
+      "publish": {}
+    },
+    "progression/level-up": {
+      "type": "button",
+      "publish": {}
+    },
+    "progression/reset": {
+      "type": "button",
+      "publish": {}
+    },
+    "progression/add-xp": {
+      "type": "button",
+      "publish": {}
+    },
+    "progression/economy/add-gold": {
+      "type": "button",
+      "publish": {}
+    },
+    "progression/economy/add-hc": {
+      "type": "button",
+      "publish": {}
+    },
+    "progression/economy/reset": {
+      "type": "button",
+      "publish": {}
     }
   }
-}
 }
 """";
 
--- DebugMenuUnity/Assets/DebugMenuIO/Schema/Api.cs
+++ DebugMenuUnity/Assets/DebugMenuIO/Schema/Api.cs
@@ -1,5 +1,28 @@
+using System.Collections.Generic;
+
+
 namespace DebugMenuIO.Schema {
     public class Api {
-        
+        public string DebugMenuApi { get; set; }
+        public Dictionary<string, Channel> Channels { get; set; } = new();
+    }
+
+    public class Channel {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public Payload Subscribe { get; set; }
+        public Payload Publish { get; set; }
+    }
+
+    public class Payload {
+        public string Type { get; set; }
+        public Dictionary<string, Property> Properties { get; set; }
+    }
+
+    public class Property {
+        public string Type { get; set; }
+        public string Format { get; set; }
+        public string Description { get; set; }
     }
 }
