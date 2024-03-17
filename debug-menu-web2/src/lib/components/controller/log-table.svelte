e566633864fc921bf82d6d61cf9175283ec35a57
Daniel Månsson <daniel.maansson@gmail.com>
2024-03-17 15:44:29 +0100

    log details

--- debug-menu-web2/src/lib/components/controller/log-table.svelte
+++ debug-menu-web2/src/lib/components/controller/log-table.svelte
@@ -6,6 +6,7 @@
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import Separator from '../ui/separator/separator.svelte';
 	export let label: string;
 	export let settings = {
 		color: 'white'
@@ -23,8 +24,9 @@
 	$: logs = $history ?? [];
 	$: filteredLogs = logs.filter(
 		(entry, index) =>
+			entry === selectedEntry || (
 			(!pauseIndex || index < pauseIndex) &&
-			entry.message.toLowerCase().includes(filterString.toLowerCase())
+			entry.message.toLowerCase().includes(filterString.toLowerCase()))
 	);
 
 	function getTimeWithMilliseconds(date: Date) {
@@ -67,6 +69,16 @@
 		let file = content.join('\n');
 		download('test.txt', file);
 	}
+
+	let selectedEntry: LogEntry | undefined = undefined;
+	function selectRow(entry: LogEntry, index: number) {
+		if(selectedEntry === entry) {
+			selectedEntry = undefined;
+		} else {
+			selectedEntry = entry;
+		}
+		filteredLogs = filteredLogs;
+	}
 </script>
 
 <div class="mb-2 flex gap-2">
@@ -98,29 +110,42 @@
 		<Button on:click={downloadFullLog} variant="outline">Download</Button>
 	</div>
 </div>
-<ScrollArea class="h-[80vh] ">
-	<Table.Root>
-		<Table.Header>
-			<Table.Row>
-				<Table.Head class="w-0">Timestamp</Table.Head>
-				<Table.Head class="w-0">Type</Table.Head>
-				<Table.Head>Message</Table.Head>
-			</Table.Row>
-		</Table.Header>
-		<Table.Body>
-			{#each filteredLogs.filter((m, index) => index >= filteredLogs.length - maxVisible) as entry, i (i)}
-				<Table.Row class={i % 2 ? '' : ''}>
-					<Table.Cell class="py-1 font-mono font-medium opacity-50"
-						>{getTimeWithMilliseconds(new Date(entry.timestamp))}</Table.Cell
-					>
-					<Table.Cell class="py-1 {getTypeClass(entry.type)}">
-						{entry.type}
-					</Table.Cell>
-					<Table.Cell class="w-full py-1">
-						{entry.message}
-					</Table.Cell>
+<div class="h-[80vh]">
+	<ScrollArea class={selectedEntry ? 'h-[60vh]' : 'h-[80vh]'}>
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-0">Timestamp</Table.Head>
+					<Table.Head class="w-0">Type</Table.Head>
+					<Table.Head>Message</Table.Head>
 				</Table.Row>
-			{/each}
-		</Table.Body>
-	</Table.Root>
-</ScrollArea>
+			</Table.Header>
+			<Table.Body>
+				{#each filteredLogs.filter((m, index) => index >= filteredLogs.length - maxVisible) as entry, i (i)}
+					<Table.Row
+						on:click={() => selectRow(entry, i)}
+						class={entry === selectedEntry ? 'bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700' : ''}
+					>
+						<Table.Cell class="py-1 font-mono font-medium opacity-50"
+							>{getTimeWithMilliseconds(new Date(entry.timestamp))}</Table.Cell
+						>
+						<Table.Cell class="py-1 {getTypeClass(entry.type)}">
+							{entry.type}
+						</Table.Cell>
+						<Table.Cell class="w-full py-1">
+							{entry.message}
+						</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</ScrollArea>
+	{#if selectedEntry}
+		<div class="h-[20vh]">
+			<Separator></Separator>
+			<ScrollArea class="h-full">
+				<div class="whitespace-pre-wrap font-mono">{selectedEntry.details}</div>
+			</ScrollArea>
+		</div>
+	{/if}
+</div>
--- debug-menu-web2/src/routes/wip/dmapi/+page.svelte
+++ debug-menu-web2/src/routes/wip/dmapi/+page.svelte
@@ -4,6 +4,7 @@
 	import { asyncapiMockData } from './mockdata';
 	import GroupTree from '$lib/components/controller/group-tree.svelte';
 	import { buildDebugmenuElements } from '$lib/debugmenuApi';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	onMount(() => {
 		update();
@@ -22,7 +23,31 @@
 		console.log(api.categories);
 	}
 	let api: any = undefined;
+
+	function getRandomInt(max:number) {
+  		return Math.floor(Math.random() * max);
+	}
+
+	let types = ["log", "warning", "error"]
+	let messages = ["The hawk didn’t understand why the ground squirrels didn’t want to be his friend.",
+	"Toddlers feeding raccoons surprised even the seasoned park ranger.",
+	"Harrold felt confident that nobody would ever suspect his spy pigeon.",
+	"The Guinea fowl flies through the air with all the grace of a turtle."]
+	function addFakeLog() {
+		api.categories[0].groups[0].channels[0].history.update(h => {
+		h ??= [];
+		h.push({
+			message: messages[getRandomInt(messages.length)],
+			type: types[getRandomInt(types.length)],
+			details: "details "+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n"+ messages[getRandomInt(messages.length)] + "\n",
+			timestamp: Date.now()
+		});
+		return h;
+	});
+	}
 </script>
+
+<Button on:click={addFakeLog}>Add Log</Button>
 
 <div class="my-1 flex items-center justify-between">
 	<h2 class="text-3xl font-bold tracking-tight">Instance Dashboard (debugmenuapi)</h2>
