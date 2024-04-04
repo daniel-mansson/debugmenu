31485413c374e448acb779c09bf771599ac29511
Daniel Månsson <daniel.maansson@gmail.com>
2024-04-04 19:35:33 +0200

    forgot import

--- debug-menu-web2/src/lib/server/auth.ts
+++ debug-menu-web2/src/lib/server/auth.ts
@@ -6,7 +6,7 @@ const { Pool } = pkg;
 import { GitHub } from "arctic";
 import { Google } from "arctic";
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
-import { GOOGLE_ID, GOOGLE_SECRET, DB_SECRET, DB_HOST, DB_NAME, DB_USER } from "$env/static/private";
+import { GOOGLE_ID, GOOGLE_SECRET, GOOGLE_CALLBACK, DB_SECRET, DB_HOST, DB_NAME, DB_USER } from "$env/static/private";
 
 console.log(DB_USER)
 export const pool = new Pool({
